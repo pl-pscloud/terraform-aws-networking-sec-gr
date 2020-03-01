fcd0018d679455b13399a203d5aae3b34c3a805a
pswitala <p.switala@pslab.eu>
2020-03-01 12:41:57 +0100

    Fixed input vars type

--- vars.tf
+++ vars.tf
@@ -6,7 +6,7 @@ variable "pscloud_sec_name" {}
 variable "pscloud_from_cidr" {
   type = list(object({
     port          = string
-    cidr          = string
+    cidr          = list(string)
     protocol      = string
   }))
   default = []
@@ -15,7 +15,7 @@ variable "pscloud_from_cidr" {
 variable "pscloud_from_sec_gr" {
   type = list(object({
     port          = string
-    sec_gr        = string
+    sec_gr        = list(string)
     protocol      = string
   }))
   default = []
