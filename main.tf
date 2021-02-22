c9d45a5b3eafc38bf08bcdd57e9553e8936dbe96
pswitala <p.switala@pslab.eu>
2021-02-22 19:48:10 +0100

    Added egress rules and change list to map vars

--- main.tf
+++ main.tf
@@ -4,38 +4,31 @@ resource "aws_security_group" "pscloud-sec-gr" {
 
   vpc_id = var.pscloud_vpc.id
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     Name = "${var.pscloud_company}_sec_gr_${var.pscloud_sec_name}_${var.pscloud_env}"
   }
 }
 
 resource "aws_security_group_rule" "pscloud-sec-rule-cidr" {
-  count           = length(var.pscloud_from_cidr)
+  for_each        = var.pscloud_from_cidr
 
-  type            = "ingress"
-  from_port       = var.pscloud_from_cidr[count.index].from_port
-  to_port         = var.pscloud_from_cidr[count.index].to_port
-  protocol        = var.pscloud_from_cidr[count.index].protocol
-  cidr_blocks     = var.pscloud_from_cidr[count.index].val
+  type            = each.value.type
+  from_port       = each.value.from_port
+  to_port         = each.value.to_port
+  protocol        = each.value.protocol
+  cidr_blocks     = each.value.val
 
   security_group_id = aws_security_group.pscloud-sec-gr.id
 }
 
 resource "aws_security_group_rule" "pscloud-sec-rule-sec-group" {
-  count                        = length(var.pscloud_from_sec_gr)
+  for_each                     = var.pscloud_from_sec_gr
 
-  type                         = "ingress"
-  from_port                    = var.pscloud_from_sec_gr[count.index].from_port
-  to_port                      = var.pscloud_from_sec_gr[count.index].to_port
-  protocol                     = var.pscloud_from_sec_gr[count.index].protocol
-  source_security_group_id     = var.pscloud_from_sec_gr[count.index].val
+  type                         = each.value.type
+  from_port                    = each.value.from_port
+  to_port                      = each.value.to_port
+  protocol                     = each.value.protocol
+  source_security_group_id     = each.value.val
 
   security_group_id = aws_security_group.pscloud-sec-gr.id
 }
--- vars.tf
+++ vars.tf
@@ -4,21 +4,23 @@ variable "pscloud_vpc" {}
 variable "pscloud_sec_name" {}
 
 variable "pscloud_from_cidr" {
-  type = list(object({
+  type = map(object({
+    type          = string
     from_port     = string
     to_port       = string
     protocol      = string
     val           = set(string)
   }))
-  default = []
+  default = {}
 }
 
 variable "pscloud_from_sec_gr" {
-  type = list(object({
+  type = map(object({
+    type          = string
     from_port     = string
     to_port       = string
     protocol      = string
     val           = string
   }))
-  default = []
+  default = {}
 }
